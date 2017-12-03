8c6588e3a04262ca592779558ce811f4ab8469b7
Brad Lawson <bradlawson@cfl.rr.com>
2017-12-02 19:04:35 -0500

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,58 +4,105 @@
 
 $(document).ready(function(){
 
-	var wins = 0;
+var wins = 0;
+
 var losses = 0;
+
+var score = 0;
+
+var randomGeneratedNumberMinusScore = 9;
 
 var randomGeneratedNumber = Math.floor((Math.random() * 101) + 19);
 
-var iconGeneratedNumber = Math.floor((Math.random() * 12) + 1);
+var nitrogenIconGeneratedNumber = Math.floor((Math.random() * 12) + 1);
+var phosphorusIconGeneratedNumber = Math.floor((Math.random() * 12) + 1);
+var potassiumIconGeneratedNumber = Math.floor((Math.random() * 12) + 1);
+var ironIconGeneratedNumber = Math.floor((Math.random() * 12) + 1);
 
 	$("#computerRandomNumber").html(randomGeneratedNumber);
 	
-	$("#nitrogen").val(iconGeneratedNumber);
-	$("#phosphorus").val(iconGeneratedNumber);
-	$("#potassium").val(iconGeneratedNumber);
-	$("#iron").val(iconGeneratedNumber);
+	$("#nitrogen").val(nitrogenIconGeneratedNumber);
+	$("#phosphorus").val(phosphorusIconGeneratedNumber);
+	$("#potassium").val(potassiumIconGeneratedNumber);
+	$("#iron").val(ironIconGeneratedNumber);
+
+	var reset = function(){
+		score = 0;
+
+		randomGeneratedNumber = Math.floor((Math.random() * 101) + 19);
+
+		nitrogenIconGeneratedNumber = Math.floor((Math.random() * 12) + 1);
+
+		phosphorusIconGeneratedNumber = Math.floor((Math.random() * 12) + 1);
+
+		potassiumIconGeneratedNumber = Math.floor((Math.random() * 12) + 1);
+
+		ironIconGeneratedNumber = Math.floor((Math.random() * 12) + 1);
+
+		$("#computerRandomNumber").html(randomGeneratedNumber);
+	
+		$("#nitrogen").val(nitrogenIconGeneratedNumber);
+		$("#phosphorus").val(phosphorusIconGeneratedNumber);
+		$("#potassium").val(potassiumIconGeneratedNumber);
+		$("#iron").val(ironIconGeneratedNumber);
+
+		$("#displayYouWinOrLose").html("Wins and Losses")
+
+	}
+
+	var resetGame = function(){
+		wins = 0;
+		losses = 0;
+		score = 0;
+
+		$("#computerRandomNumber").html(randomGeneratedNumber);
+	
+		$("#nitrogen").val(nitrogenIconGeneratedNumber);
+		$("#phosphorus").val(phosphorusIconGeneratedNumber);
+		$("#potassium").val(potassiumIconGeneratedNumber);
+		$("#iron").val(ironIconGeneratedNumber);
+	}
 
 
-	$('#nitrogen').on('click', function(){
+	$('.button').on('click', function(){
 	
 		var btnValue = this.value;
 
 			console.log(btnValue);
 
+	
+		$("#updateScore").html(score += parseInt(btnValue));
+		if (score > randomGeneratedNumber){
+			
+			losses++;
 
+			$("#updateLosses").html(losses);
+
+			$("body").css("background", "tan");
+
+			$("#displayYouWinOrLose").html("You Lose!")
+
+			reset();
+
+		} else if ( score == randomGeneratedNumber){
+			
+			wins++;
+
+			$("#updateWins").html(wins);
+
+			$("body").css("background-color", "green");
+
+			$("#displayYouWinOrLose").html("You Win!")
+
+			reset();
+		}
+
+
+		
 	});
 
 	
-	$('#phosphorus').on('click', function(){
 	
-		var btnValue = this.value;
-
-			console.log(btnValue);
-
-
-	});
-
-	$('#potassium').on('click', function(){
-	
-		var btnValue = this.value;
-
-			console.log(btnValue);
-
-
-	});
-
-	
-	$('#iron').on('click', function(){
-	
-		var btnValue = this.value;
-
-			console.log(btnValue);
-
-
-	});
 
 
 });
